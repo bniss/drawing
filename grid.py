6cf8cc772abf3c55e939b60b33290b8129d57ed9
sailfish009 <sailfish009@gmail.com>
2020-02-20 23:09:31 +0900

    update

--- grid.py
+++ grid.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
 import numpy as np
+import json
 
-def show_grid(x, y):
-    # make an empty data set
-    data = np.ones((x, y)) * np.nan
-    # fill in some fake data
-    for j in range(3)[::-1]:
-        data[x//2 - j : x//2 + j +1, x//2 - j : x//2 + j +1] = j
+
+def show_grid(np_array):
+    array_shape = np_array.shape
+    x = array_shape[0]
+    y = array_shape[1]
+
+    # # make an empty data set
+    # data = np.ones((x, y)) * np.nan
+    # # fill in some fake data
+    # for j in range(3)[::-1]:
+    #     data[x//2 - j : x//2 + j +1, x//2 - j : x//2 + j +1] = j
 
     # make a figure + axes
     fig, ax = plt.subplots(1, 1, tight_layout=True)
@@ -24,17 +30,43 @@ def show_grid(x, y):
         ax.axvline(j, lw=2, color='k', zorder=5)
 
     # draw the boxes
-    ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, y, 0, x], zorder=0)
+    ax.imshow(np_array, interpolation='none', cmap=my_cmap, extent=[0, y, 0, x], zorder=0)
     # turn off the axis labels
     ax.axis('off')
 
-    plt.rc('grid', linestyle="-", color='black')
-    # plt.scatter(x, y)
-    plt.grid(True)
     plt.show()
+
+
+def read_json(file_path):
+    with open(file_path) as json_data:
+        return json.load(json_data)
+
 
 def main():
     print('version: {}'.format(0))
-    show_grid(4, 18)
+
+    file_path = '007bbfb7.json'
+
+    data = read_json(file_path)
+
+    print('test input: {}'.format(data['test'][0]['input']))
+
+    print('------------------------------')
+
+    print('length: {}'.format(len(data['train'])))
+
+    # print('train input: {}'.format(data['train'][0]['input']))
+
+    np_input_data = np.array( data['train'][0]['input'] )
+    print('train input: {}'.format(np_input_data))
+
+    print('train input shape: {}'.format(np_input_data.shape))
+
+    print('------------------------------')
+
+    np_output_data = np.array( data['train'][0]['output'] )
+    print('train output shape: {}'.format(np_output_data.shape))
+
+    show_grid(np_input_data)
 
 if __name__ == '__main__': main()
